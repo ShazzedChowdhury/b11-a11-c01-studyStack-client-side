3cdbf2ff2b190c63517593be4786cd3047886717
shazzed-76 <shazzed7656@gmail.com>
2025-10-22 14:59:47 +0600

    modify notifications feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -24,24 +24,31 @@ const Notifications = () => {
       {isOpen && (
         <div className="absolute right-0 mt-2 bg-base-100 shadow-lg rounded-md w-64 p-2 z-100">
           <h3 className="font-semibold mb-2">Notifications</h3>
-          <ul>
-            {notifications.slice(0, 5).map((notification) => (
-              <li key={notification?._id} className="py-3 px-2 border-b border-b-gray-900 last:border-none">
-                <Link
-                  onClick={async () => {
-                    await axiosPublic.patch(
-                      `/notifications/${notification?._id}`
-                    );
-                    setRefetch((prev) => !prev);
-                  }}
-                  to={`/assignment/${notification?.assignmentId}`}
-                  className="text-sm underline"
+          {notifications.length === 0 ? (
+            <p className="py-4 text-center text-sm">Empty</p>
+          ) : (
+            <ul>
+              {notifications.slice(0, 5).map((notification) => (
+                <li
+                  key={notification?._id}
+                  className="py-3 px-2 border-b border-b-gray-900 last:border-none"
                 >
-                  {notification.message}
-                </Link>
-              </li>
-            ))}
-          </ul>
+                  <Link
+                    onClick={async () => {
+                      await axiosPublic.patch(
+                        `/notifications/${notification?._id}`
+                      );
+                      setRefetch((prev) => !prev);
+                    }}
+                    to={`/assignment/${notification?.assignmentId}`}
+                    className="text-sm underline"
+                  >
+                    {notification.message}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       )}
     </div>
